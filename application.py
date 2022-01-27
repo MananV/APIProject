6c9aa82210b0071f5c5f7e50527ee36b97e58e23
MananV <37605151+MananV@users.noreply.github.com>
2022-01-27 14:22:56 +1100

    For Mathews to Check

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+application = app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return jsonify({"about": "Hello world!"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
